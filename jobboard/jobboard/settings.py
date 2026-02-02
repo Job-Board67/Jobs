00daa213f6a2843158fa7e2bc137a5ed02bd1bc1
Arno <kabdigaliarnur@gmail.com>
2026-02-02 22:58:46 +0500

    imgonnakms

--- jobboard/jobboard/settings.py
+++ jobboard/jobboard/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^1e=ss6)v*5yv1j#8b9i=z_&k91%6=+6v@9$by2l=&$&r3)uom
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]']
+ALLOWED_HOSTS = ["jobs-kq4h.onrender.com",'localhost', '127.0.0.1', '[::1]']
 
 
 # Application definition
