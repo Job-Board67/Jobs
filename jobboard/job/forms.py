51eee0c58b8937454da7a33f3da579fd3ab8d539
Arno <kabdigaliarnur@gmail.com>
2026-02-10 23:24:52 +0500

    barabarabara

--- jobboard/job/forms.py
+++ jobboard/job/forms.py
@@ -7,11 +7,19 @@ from .models import Profile
 
 
 class RegisterForm(UserCreationForm):
-    role = forms.ChoiceField(choices=Profile.ROLE_CHOICES, initial=Profile.STUDENT)
+    email = forms.EmailField(required=True)
+    role = forms.ChoiceField(choices=Profile.ROLE_CHOICES)
 
     class Meta:
         model = User
-        fields = ("username", "email", "role", "password1", "password2")
+        fields = ["username", "email", "role", "password1", "password2"]
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data["email"]
+        if commit:
+            user.save()
+        return user
 
 
 class ApplicationForm(forms.ModelForm):
