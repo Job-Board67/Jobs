999153180e0832c63c5a83a0021d2e08c13647a7
Arno <kabdigaliarnur@gmail.com>
2026-02-11 18:18:58 +0500

    kolbasenkodanil

--- jobboard/job/signals.py
+++ jobboard/job/signals.py
@@ -9,11 +9,8 @@ User = get_user_model()
 
 @receiver(post_save, sender=User)
 def create_or_update_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
-    else:
-        if hasattr(instance, "profile"):
-            instance.profile.save()
+    # создаст профиль если его нет; если уже есть — просто вернёт существующий
+    Profile.objects.get_or_create(user=instance)
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_profile(sender, instance, created, **kwargs):
     # создаём профиль только если его нет
